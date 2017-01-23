5b9bfec14c9d338de4382e2f2705bc36ecd460cc
Xavi <mrjavoman@hotmail.com>
2017-01-22 22:56:52 -0600

    Finishing Section 5

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = "d9eca8afc4376df2f0d34385d42da9ff";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
